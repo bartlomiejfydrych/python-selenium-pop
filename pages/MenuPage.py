8b3f760dc5e33ef7b45a7e3521d17fcaa09f44a9
bartlomiejfydrych <fydbar@gmail.com>
2020-12-06 10:42:58 +0100

    Refactoring Menu files

--- pages/MenuPage.py
+++ pages/MenuPage.py
@@ -1,59 +1,60 @@
-from selenium.webdriver import ActionChains
-
 from locators.MenuLocators import MenuLocators
 from pages.BasePage import BasePage
+from log import log, for_all_methods
 
 
+@for_all_methods(log)
 class MenuPage(MenuLocators, BasePage):
-    # TODO: Poprosić Dawida przy refactoringu by dokładnie to wszystko zbadał
-    # Trzeba było to usunąć bo nazwaklasy.actions nie działało i trzeba było dać self.
-    # actions = None
-
-    def __init__(self, driver):
-        self.driver = driver
-        self.actions = ActionChains(driver)
 
     # Metody zadania 1:------------------------------------------------------------------------------------------------
     def z1_kliknij_link2(self):
-        self.driver.find_element(*self.Z1_LINK2).click()
+        self.find(self.Z1_LINK2).click()
 
     def z1_sprawdz_alert(self):
-        assert self.driver.find_element(*self.Z1_ALERT).is_displayed()
+        assert self.find(self.Z1_ALERT).is_displayed()
 
     # Metody zadania 2:------------------------------------------------------------------------------------------------
     def z2_rozwin_liste(self):
-        self.driver.find_element(*self.Z2_ROZWIN_BUTTON).click()
+        self.find(self.Z2_ROZWIN_BUTTON).click()
 
     def z2_kliknij_link3(self):
-        self.driver.find_element(*self.Z2_LINK3).click()
+        self.find(self.Z2_LINK3).click()
 
     def z2_sprawdz_alert(self):
-        assert self.driver.find_element(*self.Z2_ALERT).is_displayed()
+        assert self.find(self.Z2_ALERT).is_displayed()
 
     # Metody zadania 3:------------------------------------------------------------------------------------------------
     def z3_kliknij_liste(self):
-        self.driver.find_element(*self.Z3_LINK3).click()
+        self.find(self.Z3_LINK3).click()
 
     def z3_kliknij_link3_c(self):
-        self.driver.find_element(*self.Z3_LINK3_C).click()
+        self.find(self.Z3_LINK3_C).click()
 
     def z3_sprawdz_alert(self):
-        self.driver.find_element(*self.Z3_ALERT).is_displayed()
+        self.find(self.Z3_ALERT).is_displayed()
 
     # Metody zadania 4:------------------------------------------------------------------------------------------------
     def z4_kliknij_link3_b(self):
-        self.actions.move_to_element(self.driver.find_element(*self.Z4_LINK3)) \
-            .move_to_element(self.driver.find_element(*self.Z4_LINK3_B)).click().perform()
+        z4_link3 = self.find(self.Z4_LINK3)
+        z4_link3_b = self.find(self.Z4_LINK3_B)
+        self.actions.move_to_element(z4_link3) \
+            .move_to_element(z4_link3_b) \
+            .click().perform()
 
     def z4_sprawdz_alert(self):
-        assert self.driver.find_element(*self.Z4_ALERT).is_displayed()
+        assert self.find(self.Z4_ALERT).is_displayed()
 
     # Metody zadania 5:------------------------------------------------------------------------------------------------
     def z5_kliknij_link3_b5(self):
-        self.actions.move_to_element(self.driver.find_element(*self.Z5_LINK3)) \
-            .move_to_element(self.driver.find_element(*self.Z5_LINK3_B)) \
-            .move_to_element(self.driver.find_element(*self.Z5_LINK3_B1)) \
-            .move_to_element(self.driver.find_element(*self.Z5_LINK3_B5)).click().perform()
+        z5_link3 = self.find(self.Z5_LINK3)
+        z5_link3_b = self.find(self.Z5_LINK3_B)
+        z5_link3_b1 = self.find(self.Z5_LINK3_B1)
+        z5_link3_b5 = self.find(self.Z5_LINK3_B5)
+        self.actions.move_to_element(z5_link3) \
+            .move_to_element(z5_link3_b) \
+            .move_to_element(z5_link3_b1) \
+            .move_to_element(z5_link3_b5) \
+            .click().perform()
 
     def z5_sprawdz_alert(self):
-        assert self.driver.find_element(*self.Z5_ALERT).is_displayed()
+        assert self.find(self.Z5_ALERT).is_displayed()
