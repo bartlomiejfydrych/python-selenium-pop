57a835cc024bb1b801a7098bbb97f26890c211ac
bartlomiejfydrych <fydbar@gmail.com>
2020-12-09 16:17:45 +0100

    Refactoring Przeciagnij Upusc files

--- pages/PrzeciagnijUpuscPage.py
+++ pages/PrzeciagnijUpuscPage.py
@@ -2,21 +2,20 @@ from selenium.webdriver import ActionChains
 
 from locators.PrzeciagnijUpuscLocators import PrzeciagnijUpuscLocators
 from configurations import SCRIPT_PATH
+from log import log, for_all_methods
+from pages.BasePage import BasePage
 
 
-class PrzeciagnijUpuscPage(PrzeciagnijUpuscLocators):
-
-    def __init__(self, driver):
-        self.driver = driver
-        self.actions = ActionChains(driver)
+@for_all_methods(log)
+class PrzeciagnijUpuscPage(PrzeciagnijUpuscLocators, BasePage):
 
     # Metody zadania 1:------------------------------------------------------------------------------------------------
     def z1_przenies_elementy_do_grupy_b(self):
-        for elements in self.driver.find_elements(*self.Z1_ELEMENTY):
-            self.actions.drag_and_drop(elements, self.driver.find_element(*self.Z1_GRUPA_B)).perform()
+        for elements in self.find_all(self.Z1_ELEMENTY):
+            self.actions.drag_and_drop(elements, self.find(self.Z1_GRUPA_B)).perform()
 
     def z1_sprawdz_alert_1(self):
-        assert self.driver.find_element(*self.Z1_ALERT).is_displayed()
+        assert self.find(self.Z1_ALERT).is_displayed()
 
     # Metody zadania 2:------------------------------------------------------------------------------------------------
     def z2_posortuj_elementy(self):
@@ -33,15 +32,15 @@ class PrzeciagnijUpuscPage(PrzeciagnijUpuscLocators):
             smooth_move.release().perform()
 
     def z2_sprawdz_alert_2(self):
-        assert self.driver.find_element(*self.Z2_ALERT).is_displayed()
+        assert self.find(self.Z2_ALERT).is_displayed()
 
     # Metody zadania 3:------------------------------------------------------------------------------------------------
     def z3_przenies_elementy_do_grupy_y_html5(self):
         with open(SCRIPT_PATH) as f:
             full_script = f"{f.read()} \nsimulateHTML5DragAndDrop(arguments[0], arguments[1])"
 
-        for element in self.driver.find_elements(*self.Z3_ELEMENTY):
-            self.driver.execute_script(full_script, element, self.driver.find_element(*self.Z3_GRUPA_Y))
+        for element in self.find_all(self.Z3_ELEMENTY):
+            self.driver.execute_script(full_script, element, self.find(self.Z3_GRUPA_Y))
 
     def z3_sprawdz_alert_3(self):
-        assert self.driver.find_element(*self.Z3_ALERT).is_displayed()
+        assert self.find(self.Z3_ALERT).is_displayed()
--- tests/test_przeciagnij_upusc.py
+++ tests/test_przeciagnij_upusc.py
@@ -1,12 +1,12 @@
 from tests.TestBase import TestBase
 
 
-class PrzeciagnijUpuscTest(TestBase):
+class TestPrzeciagnijUpusc(TestBase):
     przeciagnij_upusc_page = None
 
-    def setUp(cls):
-        super().setUp()
-        cls.przeciagnij_upusc_page = cls.glowna_page.go_to_przeciagnij_upusc_page()
+    def setup_method(self):
+        super().setup_method()
+        self.przeciagnij_upusc_page = self.glowna_page.go_to_przeciagnij_upusc_page()
 
     def test_zadanie1(self):
         self.przeciagnij_upusc_page.z1_przenies_elementy_do_grupy_b()
@@ -21,9 +21,6 @@ class PrzeciagnijUpuscTest(TestBase):
         self.przeciagnij_upusc_page.z3_sprawdz_alert_3()
 
     def test_wszystkie_zadania(self):
-        self.przeciagnij_upusc_page.z1_przenies_elementy_do_grupy_b()
-        self.przeciagnij_upusc_page.z1_sprawdz_alert_1()
-        self.przeciagnij_upusc_page.z2_posortuj_elementy()
-        self.przeciagnij_upusc_page.z2_sprawdz_alert_2()
-        self.przeciagnij_upusc_page.z3_przenies_elementy_do_grupy_y_html5()
-        self.przeciagnij_upusc_page.z3_sprawdz_alert_3()
+        self.test_zadanie1()
+        self.test_zadanie2()
+        self.test_zadanie3()
